02e7db350465f051c1f8eccd9cac9bfbdec24d4a
leslie.alldridge <leslie.alldridge@gmail.com>
2020-04-18 19:26:46 +1200

    frontend running on antd

---
+++ client/src/context/AppReducer.js
@@ -0,0 +1,36 @@
+import {
+  DELETE_TRANSACTION,
+  ADD_TRANSACTION,
+  GET_TRANSACTIONS,
+  TRANSACTION_ERROR,
+} from './constants';
+
+export default (state, action) => {
+  switch (action.type) {
+    case GET_TRANSACTIONS:
+      return {
+        ...state,
+        loading: false,
+        transactions: action.payload,
+      };
+    case DELETE_TRANSACTION:
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction._id !== action.payload
+        ),
+      };
+    case ADD_TRANSACTION:
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
+    case TRANSACTION_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
---
+++ client/src/context/constants.js
@@ -0,0 +1,4 @@
+export const DELETE_TRANSACTION = () => 'DELETE_TRANSACTION';
+export const ADD_TRANSACTION = () => 'ADD_TRANSACTION';
+export const GET_TRANSACTIONS = () => 'GET_TRANSACTIONS';
+export const TRANSACTION_ERROR = () => 'TRANSACTION_ERROR';
